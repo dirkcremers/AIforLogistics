626c09ee0cfd48968970c27d63ac5f94012080e2
dirkc <dirkcremers29@gmail.com>
2023-02-24 14:02:03 +0100

    start on assignment 1 - coding part

---
+++ main.py
@@ -0,0 +1,186 @@
+import numpy as np
+from scipy.stats import gamma
+import pandas as pd
+
+def inventory_fill(mu: np.array, variance: np.array) -> np.array:
+    inventory = mu + 1.97 * np.sqrt(variance)
+
+    # TODO: check if inventory should be rounded up
+    return np.ceil(inventory)
+
+
+def descretized_demand_distribution(mu: float, variance: float):
+    """Returns a discrete distribution of the demand.
+
+    Args:
+        mu (float): mean of the demand
+        sigma (float): standard deviation of the demand
+
+    Returns:
+        np.ndarray: discrete distribution of the demand
+    """
+
+    # scale = variance / mu
+    # shape = mu / scale
+
+    # TODO: check if variance is equal to the scale in scipy cdf
+
+    a = gamma.cdf(np.arange(0, 11), mu, scale=np.sqrt(variance))
+    a[-1] = 1.0
+    b = np.diff(a)
+    b = np.insert(b, 0, a[0])
+    return b
+
+
+def routing_cost(actions: np.array) -> float:
+    """Calculates the routing cost.
+
+    Args:
+        actions (np.array): actions to take
+
+    Returns:
+        float: routing cost
+    """
+    routes_to_drive = actions > 0
+    if np.sum(routes_to_drive) == 3:
+        return 500
+    elif np.sum(abs(routes_to_drive - np.array([1, 1, 0]))) == 0:
+        return 60
+    elif np.sum(routes_to_drive) == 2:
+        return 75
+    elif np.sum(abs(routes_to_drive - np.array([0, 0, 1]))) == 0:
+        return 55
+    elif np.sum(routes_to_drive) == 1:
+        return 40
+    else:
+        return 0
+
+
+def policy_evaluation(GAMMA: float) -> np.array:
+
+    V = np.zeros((11, 11, 11))
+
+    inventory_up_to = inventory_fill(mu, variance)
+
+    store_1_demand = descretized_demand_distribution(mu[0], variance[0])
+    store_2_demand = descretized_demand_distribution(mu[1], variance[1])
+    store_3_demand = descretized_demand_distribution(mu[2], variance[2])
+
+    iteration = 1
+
+    while True:
+        print(iteration)
+
+        V_old = V.copy()
+
+        for store1 in range(11):
+            for store2 in range(11):
+                for store3 in range(11):
+                    actions = inventory_up_to - np.array([store1, store2, store3])
+                    actions[actions < 0] = 0
+
+                    value = 0
+
+                    # TODO: check if demand per store is independent
+
+                    for demand1, prob1 in enumerate(store_1_demand):
+                        for demand2, prob2 in enumerate(store_2_demand):
+                            for demand3, prob3 in enumerate(store_3_demand):
+                                prob = prob1 * prob2 * prob3
+
+                                new_stores = inventory_up_to - np.array([demand1, demand2, demand3])
+
+                                shortage = np.sum(new_stores[new_stores < 0])
+                                holding = np.sum(new_stores[new_stores >= 0])
+
+                                # set negative values to zero
+                                new_stores[new_stores < 0] = 0
+                                new_stores = new_stores.astype(int)
+
+                                reward = shortage * -19 + holding * -1 - routing_cost(actions)
+
+                                value += prob * (reward + GAMMA * V[new_stores[0], new_stores[1], new_stores[2]])
+
+                    V[store1, store2, store3] = value
+
+        iteration += 1
+        if np.max(np.abs(V - V_old)) < 0.01:
+            break
+
+    return V
+
+
+def policy_evaluation_single_step(GAMMA: float) -> np.array:
+    inventory_up_to = inventory_fill(mu, variance)
+
+    best_actions = np.zeros((11, 11, 11))
+
+    V = policy_evaluation(GAMMA)
+
+    store_1_demand = descretized_demand_distribution(mu[0], variance[0])
+    store_2_demand = descretized_demand_distribution(mu[1], variance[1])
+    store_3_demand = descretized_demand_distribution(mu[2], variance[2])
+
+    iteration = 1
+
+    while iteration < 2:
+        print(iteration)
+        iteration += 1
+
+        for store1 in range(11):
+            for store2 in range(11):
+                for store3 in range(11):
+
+                    best_value = -np.inf
+                    best_action = -10
+
+                    for action1 in range(0, 10-store1):
+                        for action2 in range(0, 10-store2):
+                            for action3 in range(0, 10-store3):
+                                actions = np.array([action1, action2, action3])
+
+                                value = 0
+
+                                for demand1, prob1 in enumerate(store_1_demand):
+                                    for demand2, prob2 in enumerate(store_2_demand):
+                                        for demand3, prob3 in enumerate(store_3_demand):
+                                            prob = prob1 * prob2 * prob3
+
+                                            new_stores = inventory_up_to - np.array([demand1, demand2, demand3])
+
+                                            shortage = np.sum(new_stores[new_stores < 0])
+                                            holding = np.sum(new_stores[new_stores >= 0])
+
+                                            # set negative values to zero
+                                            new_stores[new_stores < 0] = 0
+                                            new_stores = new_stores.astype(int)
+
+                                            reward = shortage * -19 + holding * -1 - routing_cost(actions)
+
+                                            value += prob * (
+                                                        reward + GAMMA * V[new_stores[0], new_stores[1], new_stores[2]])
+
+                                if value > best_value:
+                                    best_value = value
+                                    best_action = actions
+
+                    best_actions[store1, store2, store3] = best_action
+
+    return best_actions
+
+
+
+
+
+
+
+
+
+GAMMA= 0.8
+mu = np.array([3, 5, 2])
+variance = np.array([1, 2, 3])
+# V = policy_evaluation(GAMMA)
+Q = policy_evaluation_single_step(GAMMA)
+print(Q)
+# pd.DataFrame(V).to_csv('ValueIteration.csv')
+
