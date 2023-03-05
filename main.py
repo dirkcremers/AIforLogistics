a96004597f067907b1f01ecdb21836aace46da22
dirkc <dirkcremers29@gmail.com>
2023-03-05 09:45:47 +0100

    update Q5

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import pandas as pd
+import xlsxwriter
 import numpy as np
 from scipy.stats import gamma
+
 
 def inventory_fill(mu: np.array, variance: np.array) -> np.array:
     inventory = mu + 1.97 * np.sqrt(variance)
 
-    # TODO: check if inventory should be rounded up
     return np.ceil(inventory)
 
 
@@ -21,8 +23,6 @@ def descretized_demand_distribution(mu: float, variance: float):
 
     scale = variance / mu
     shape = mu / scale
-
-    # TODO: check if variance is equal to the scale in scipy cdf
 
     a = gamma.cdf(np.arange(0, 11), shape, scale=scale)
     a[-1] = 1.0
@@ -56,7 +56,6 @@ def routing_cost(actions: np.array) -> float:
 
 
 def policy_evaluation(GAMMA: float) -> np.array:
-
     V = np.zeros((11, 11, 11))
 
     inventory_up_to = inventory_fill(mu, variance)
@@ -80,8 +79,6 @@ def policy_evaluation(GAMMA: float) -> np.array:
 
                     value = 0
 
-                    # TODO: check if demand per store is independent
-
                     for demand1, prob1 in enumerate(store_1_demand):
                         for demand2, prob2 in enumerate(store_2_demand):
                             for demand3, prob3 in enumerate(store_3_demand):
@@ -89,7 +86,7 @@ def policy_evaluation(GAMMA: float) -> np.array:
 
                                 new_stores = inventory_up_to - np.array([demand1, demand2, demand3])
 
-                                shortage = np.sum(new_stores[new_stores < 0])
+                                shortage = -np.sum(new_stores[new_stores < 0])
                                 holding = np.sum(new_stores[new_stores >= 0])
 
                                 # set negative values to zero
@@ -110,7 +107,6 @@ def policy_evaluation(GAMMA: float) -> np.array:
 
 
 def policy_evaluation_single_step(GAMMA: float) -> np.array:
-    inventory_up_to = inventory_fill(mu, variance)
 
     best_actions = np.zeros((11, 11, 11))
 
@@ -133,9 +129,9 @@ def policy_evaluation_single_step(GAMMA: float) -> np.array:
                     best_value = -np.inf
                     best_action = -10
 
-                    for action1 in range(0, 10-store1):
-                        for action2 in range(0, 10-store2):
-                            for action3 in range(0, 10-store3):
+                    for action1 in range(0, 11):
+                        for action2 in range(0, 11):
+                            for action3 in range(0, 11):
                                 actions = np.array([action1, action2, action3])
 
                                 value = 0
@@ -145,9 +141,11 @@ def policy_evaluation_single_step(GAMMA: float) -> np.array:
                                         for demand3, prob3 in enumerate(store_3_demand):
                                             prob = prob1 * prob2 * prob3
 
-                                            new_stores = inventory_up_to - np.array([demand1, demand2, demand3])
+                                            new_stores = np.array([min(store1 + action1, 10) - demand1,
+                                                                   min(store2 + action2, 10) - demand2,
+                                                                   min(store3 + action3, 10) - demand3])
 
-                                            shortage = np.sum(new_stores[new_stores < 0])
+                                            shortage = -np.sum(new_stores[new_stores < 0])
                                             holding = np.sum(new_stores[new_stores >= 0])
 
                                             # set negative values to zero
@@ -157,7 +155,7 @@ def policy_evaluation_single_step(GAMMA: float) -> np.array:
                                             reward = shortage * -19 + holding * -1 - routing_cost(actions)
 
                                             value += prob * (
-                                                        reward + GAMMA * V[new_stores[0], new_stores[1], new_stores[2]])
+                                                    reward + GAMMA * V[new_stores[0], new_stores[1], new_stores[2]])
 
                                 if value > best_value:
                                     best_value = value
@@ -168,14 +166,7 @@ def policy_evaluation_single_step(GAMMA: float) -> np.array:
     return best_actions
 
 
-
-
-
-
-
-
-
-GAMMA= 0.8
+GAMMA = 0.8
 mu = np.array([3, 5, 2])
 variance = np.array([1, 2, 3])
 
@@ -185,8 +176,82 @@ print(prob)
 V = policy_evaluation(GAMMA)
 print(V)
 
-
 # Q = policy_evaluation_single_step(GAMMA)
 # print(Q)
 # pd.DataFrame(V).to_csv('ValueIteration.csv')
 
+# %%
+
+Q = np.zeros((11, 11, 11, 11, 11, 11))
+n_episodes = 1000000
+len_episode = 75
+epsilon = 0.05
+Q[10, 10, 10, 1, 2, 3] = 1
+alpha = 0.01
+
+store_1_demand = descretized_demand_distribution(mu[0], variance[0])
+store_2_demand = descretized_demand_distribution(mu[1], variance[1])
+store_3_demand = descretized_demand_distribution(mu[2], variance[2])
+
+for iteration in range(n_episodes):
+    print(f'({iteration}/{n_episodes})')
+    store1, store2, store3 = np.random.randint(0, 11, 3)  # start with random initial inventory for convergence purposes
+    for _ in range(len_episode):
+        # take action epsilon greedy
+        if np.random.uniform(0, 1) < epsilon:
+            action1, action2, action3 = np.random.randint(0, 11, 3)
+        else:
+            action1, action2, action3 = np.unravel_index(np.argmax(Q[store1, store2, store3, :, :, :]),
+                                                         Q[store1, store2, store3, :, :, :].shape)
+        demand1 = np.random.choice(np.arange(0, 11), p=store_1_demand)
+        demand2 = np.random.choice(np.arange(0, 11), p=store_2_demand)
+        demand3 = np.random.choice(np.arange(0, 11), p=store_3_demand)
+
+        new_stores = np.array([min(store1 + action1, 10) - demand1, min(store2 + action2, 10) - demand2,
+                               min(store3 + action3, 10) - demand3])
+
+        shortage = -np.sum(new_stores[new_stores < 0])
+        holding = np.sum(new_stores[new_stores >= 0])
+
+        # set negative values to zero
+        new_stores[new_stores < 0] = 0
+        new_stores = new_stores.astype(int)
+
+        reward = shortage * -19 + holding * -1 - routing_cost(np.array([action1, action2, action3]))
+
+        Q[store1, store2, store3, action1, action2, action3] += alpha * (
+                    reward + GAMMA * np.max(Q[new_stores[0], new_stores[1], new_stores[2], :, :, :]) - Q[
+                store1, store2, store3, action1, action2, action3])
+
+        store_1, store2, store3 = new_stores
+
+policy = np.zeros((11, 11, 11), dtype=object)
+V = np.zeros((11, 11, 11))
+for store1 in range(11):
+    for store2 in range(11):
+        for store3 in range(11):
+            policy[store1, store2, store3] = np.unravel_index(np.argmax(Q[store1, store2, store3, :, :, :]),
+                                                              Q[store1, store2, store3, :, :, :].shape)
+            V[store1, store2, store3] = np.max(Q[store1, store2, store3, :, :, :])
+
+
+# Save results to excel
+
+# Policy
+
+writer = pd.ExcelWriter('Q_learning_policy.xlsx', engine='xlsxwriter')
+
+for i in range(0, 11):
+    df = pd.DataFrame(policy[:,:,i])
+    df.to_excel(writer, sheet_name='bin%d' % i)
+
+writer.save()
+
+# Value function
+writer = pd.ExcelWriter('Q_learning_V.xlsx', engine='xlsxwriter')
+
+for i in range(0, 11):
+    df = pd.DataFrame(V[:,:,i])
+    df.to_excel(writer, sheet_name='bin%d' % i)
+
+writer.save()
