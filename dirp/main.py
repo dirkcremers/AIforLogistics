3bf4bf164a28fe22d9bf63ed745afb0d7b743d90
dirkc <dirkcremers29@gmail.com>
2023-03-29 12:23:28 +0200

    small cleanup for code

--- dirp/main.py
+++ dirp/main.py
@@ -1,45 +1,65 @@
 import numpy as np
 import pandas as pd
+from stable_baselines3 import PPO
 
 from dirp.AI4LEnvironment import AI4LEnvironment
+from dirp.PPO_env import PPO_env
 from dirp.genetic_algorithm import GeneticAlgorithm
 
-# create dataframe for saving results
-df = pd.DataFrame(columns=['iteration', 'inventory', 'action', 'reward', 'avgCost', 'HoldCost', 'LostCost', 'RoutingCost'])
 
-# create the environment
-env = AI4LEnvironment()
+def run_simulation(policy: str) -> None:
+    # create dataframe for saving results
+    df = pd.DataFrame(columns=['iteration', 'inventory', 'action', 'reward', 'avgCost', 'HoldCost', 'LostCost', 'RoutingCost'])
 
-# reset the environment
-obs = env.reset()
-iteration = 0
-done = False
+    # create the environment
+    env = AI4LEnvironment()
 
-while done == False:
-    print('\n', iteration, '------------------------------------')
+    # reset the environment
+    obs = env.reset()
+    iteration = 0
+    done = False
 
-    # create the genetic algorithm
-    gen = GeneticAlgorithm(env)
-    action = gen.run()
+    while done == False:
+        print('\n', iteration, '------------------------------------')
 
-    # SS policy
-    # action = np.less_equal(obs, env.s).astype(int)
-    # action[0] = 1
-    # print(action)
+        # SS policy
+        if policy == 'SS':
+            # action = np.less_equal(obs, env.s).astype(int)
+            # action[0] = 1
+            # print(action)
 
-    # copy inventory before demand and action are taken
-    obs_old = obs.copy()
+        # Genetic Algorithm policy
+        if policy == 'GA':
+            gen = GeneticAlgorithm(env)
+            action = gen.run()
 
-    # take the action
-    obs, reward, done, info = env.step(action)
-    print('inventory: ', obs)
-    print('reward: ', reward)
-    print('average cost: ', env.avgCost)
+        # PPO policy
+        if policy == 'PPO':
+            model = None
+            if iteration == 0:
+                env_PPO = PPO_env()
+                # env = make_vec_env('PPO-v0', n_envs=4)
+                model = PPO('MlpPolicy', env_PPO, learning_rate=0.001, verbose=0)
+                model.learn(total_timesteps=60000)
+                model.save("ppo_dirp")
+            action, _states = model.predict(obs, deterministic=False)
 
-    # save results
-    df = df.append({'iteration': iteration, 'inventory': obs_old, 'action': action, 'reward': reward, 'avgCost': env.avgCost, 'HoldCost': info['HoldingCost'], 'LostCost': info['LostCost'], 'RoutingCost': info['TransportationCost']}, ignore_index=True)
+        # copy inventory before demand and action are taken
+        obs_old = obs.copy()
 
-    iteration += 1
+        # take the action
+        obs, reward, done, info = env.step(action)
+        print('inventory: ', obs)
+        print('reward: ', reward)
+        print('average cost: ', env.avgCost)
+
+        # save results
+        df = df.append({'iteration': iteration, 'inventory': obs_old, 'action': action, 'reward': reward, 'avgCost': env.avgCost, 'HoldCost': info['HoldingCost'], 'LostCost': info['LostCost'], 'RoutingCost': info['TransportationCost']}, ignore_index=True)
+
+        iteration += 1
+
+    df.to_csv(f'results_{policy}.csv', index=False)
 
 
-df.to_csv('results_genetic.csv', index=False)
+if __name__ == '__main__':
+    run_simulation('PPO')
