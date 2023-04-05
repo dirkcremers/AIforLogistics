d849477657fe6d6389007c3d92997a1730031bf6
dirkc <dirkcremers29@gmail.com>
2023-04-05 12:48:13 +0200

    start on NN

--- dirp/NNRoutingCost.py
+++ dirp/NNRoutingCost.py
@@ -10,6 +10,10 @@ env = RoutingCostEnv(settings)
 # create empty dataframe
 df = pd.DataFrame(columns=['action', 'inventory', 'orders', 'routing_cost'])
 
+# create empty array
+input = np.zeros(20)
+output = np.zeros(1)
+
 
 for _ in range(10):
     # sample action
@@ -25,7 +29,32 @@ for _ in range(10):
 
     df = df.append({'action': action, 'inventory': inventory, 'orders': orders, 'routing_cost': routing_cost}, ignore_index=True)
 
+    # add orders to ordering in a new row
+    input = np.vstack((input, orders))
+    output = np.vstakc((output, routing_cost))
+
+
 a = 1
+# create NN which with input the orders and as output the routing cost
 
+from keras.models import Sequential
+from keras.layers import Dense
 
-# create NN which has input
+# define the neural network model
+model = Sequential()
+model.add(Dense(10, input_dim=20, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+# compile the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# define the input and output data
+X = [[0, 12, 11]]
+Y = [-100]
+
+# train the model
+model.fit(X, Y, epochs=1000, verbose=0)
+
+# make a prediction
+result = model.predict([[0, 12, 11]])
+print(result)
